b041a72b7a8689683d907963989e78597c509e69
IamShihabPro <mshihab.pro@gmail.com>
2024-03-07 21:36:23 +0600

    hospital server

---
+++ controllers/authController.js
@@ -0,0 +1,76 @@
+import User from '../models/UserSchema.js';
+import Doctor from '../models/DoctorSchema.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+// Register
+export const register = async (req, res) => {
+    const { email, password, name, role, photo, gender } = req.body;
+    try {
+        let user = null;
+        if (role === 'patient') {
+            user = await User.findOne({ email });
+        } else if (role === 'doctor') {
+            user = await Doctor.findOne({ email });
+        }
+        // Handle user registration logic
+
+        // check user exist
+        if(user){
+            return res.status(400).json({message: 'User already exists'})
+        }
+
+        // hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password, salt)
+
+        if(role === 'patient'){
+            user = new User({
+                name, email, password: hashPassword, photo, gender,role
+            })
+        }
+        else if(role === 'doctor'){
+            user = new Doctor({
+                name, email, password: hashPassword, photo, gender,role
+            })
+        }
+
+        await user.save()
+        res.status(200).json({success: true, message: "User successfully created"})
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+// Login
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let user = null;
+        if (req.body.role === 'patient') {
+            user = await User.findOne({ email });
+        } else if (req.body.role === 'doctor') {
+            user = await Doctor.findOne({ email });
+        }
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        // Generate JWT token for authenticated user
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({ success: true, token });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+import authRoute from './routes/auth.js'
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 5000
+const corsOptions = { origin: true };
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lgdhrpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log('MongoDB connected')
+    } catch(err){
+        console.log('MongoDB connection failed')
+    }
+}
+
+// middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser())
+// app.use('api/v1/auth', authRoute)
+app.use('/api/v1/auth', authRoute);
+
+app.get('/', (req, res) => {
+    res.send('Hospital Server is Running');
+});
+
+app.listen(port, () => {
+    connectDB()
+    console.log(`server is running on port ${port}`);
+});
