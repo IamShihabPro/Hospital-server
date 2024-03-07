7490d3a9abc0ed3ad02d8ad662fefee5ddc21be4
IamShihabPro <mshihab.pro@gmail.com>
2024-03-08 01:31:51 +0600

    crud

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,49 @@
 import User from '../models/UserSchema.js'
 
+// update
 export const updateUser = async(req, res) =>{
     const id = req.params.id
     try{
-        const updatedUser = await User.findByIdAndUpdate(id, {$set: req.body}, {new:true})
-        res.status(200).json({success:true, message:'Successfully updated', data:updatedUser})
+        const updatedUser = await User.findByIdAndUpdate(id, {$set: req.body}, {new: true})
+        res.status(200).json({success: true, message:'Successfully updated', data: updatedUser})
     }
     catch(err){
-        res.status(500).json({success:false, message:'failed to updated'})
+        res.status(500).json({success: false, message:'failed to updated'})
+    }
+}
+
+// delete
+export const deleteUser = async(req, res) =>{
+    const id = req.params.id
+    try{
+        await User.findByIdAndDelete(id)
+        res.status(200).json({success: true, message:'Successfully deleted'})
+    }
+    catch(err){
+        res.status(500).json({success: false, message:'failed to delete'})
+    }
+}
+
+
+// single user
+export const getSingleUser = async(req, res) =>{
+    const id = req.params.id
+    try{
+        const user = await User.findById(id)
+        res.status(200).json({success: true, message:'User found', data: user})
+    }
+    catch(err){
+        res.status(500).json({success: false, message:'No user found'})
+    }
+}
+
+// all user
+export const getAllUser = async(req, res) =>{
+    try{
+        const users = await User.find({})
+        res.status(200).json({success: true, message:'Users found', data: users})
+    }
+    catch(err){
+        res.status(500).json({success: false, message:'No users found'})
     }
 }
