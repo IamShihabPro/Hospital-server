584e8f93b1657c0c4e611e81f709c5349c1f86fa
IamShihabPro <mshihab.pro@gmail.com>
2024-03-08 17:05:54 +0600

    crud

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import {updateUser, deleteUser, getSingleUser, getAllUser} from '../controllers/userController.js'
+import {authenticate} from '../auth/verifyToken.js'
 
 const router = express.Router()
 router.get('/', getAllUser)
-router.get('/:id', getSingleUser)
+router.get('/:id', authenticate, getSingleUser)
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
 
